50886d009f114411485c35fef266621d4e7bc519
Gean Carlos de Sousa Bandeira <60990296+geanbandeira@users.noreply.github.com>
2024-10-13 21:32:09 -0300

    Update DebounceFunction.js

--- DebounceFunction.js
+++ DebounceFunction.js
@@ -2,5 +2,12 @@
 function debounce(fn, delay){
   let timeout;
   clearTimeout(timeout);
-  
+  timeout = setTimeout(() => fn(args...), delay);
+};
 }
+
+const handleInput = debounce((e) =>{
+  console.log(e.target.value);
+}, 300);
+
+document.querySelector('input').addEventListener('input', handleInput);
